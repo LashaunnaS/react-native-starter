051ec8164a5809725a3afaab008037c5bca35d38
Lashaunna Samuels <lsamuels@Lashaunna-Samuels-MacBook-Pro.local>
2022-06-06 20:10:45 -0400

    initial app setup.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import {NavigationContainer} from '@react-navigation/native';
+import React from 'react';
+import {View} from 'react-native';
+
+const App = (): JSX.Element => {
+  return (
+    <NavigationContainer>
+      <View />
+    </NavigationContainer>
+  );
+};
+
+export default App;
